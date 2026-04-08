f4e48e66312f254002c67d7501c9b7b4c919b0ec
Emerson Azevedo Aguiar <eazevedo@student.ethz.ch>
2026-04-08 08:40:43 +0200

    Ex4. Completed

--- hw4_reinforcement_learning/exercises/ex4_sac_config.py
+++ hw4_reinforcement_learning/exercises/ex4_sac_config.py
@@ -1,12 +1,12 @@
 SAC_PARAMETERS = {
     "seed": 42,
     "hidden_sizes": [256, 128, 128],
-    "total_iterations": 2048, # total number of training iterations
-    "learning_start_steps": 1000, # number of env steps to collect before training
+    "total_iterations": 5_000, # total number of training iterations
+    "learning_start_steps": 5000, # number of env steps to collect before training
     "train_freq": 500, # number of env steps between SAC updates
     "gradient_steps": 200, # number of gradient updates per SAC update
     "batch_size": 1024, # batch size for SAC update
-    "eval_freq": 2048, # number of env steps between evaluations
+    "eval_freq": 5000, # number of env steps between evaluations
     "replay_size": 1_000_000,
     "gamma": 0.99,
     "tau": 0.005,
